eabecb6f4130c8a3ecbdc10495a5e245e6ec1db0
Frantisek Cerny <ferry.cerny@gmail.com>
2013-07-13 14:19:57 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
 
-var buffer = fs.readFileSync(index.html);
+var buffer = new Buffer("Hello Word from index.html",25);
 
+fs.readFileSync(index.html,"utf-8");
 console.log(buf.toString());
+response.send(resultOfreadFileSync);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
