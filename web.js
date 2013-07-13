afc07cf5a512b44b5f020574f1ef532eeebc85b1
Frantisek Cerny <ferry.cerny@gmail.com>
2013-07-13 14:06:06 +0000

    Update web.js

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+var buffer = fs.readFileSync(index.html);
+
+console.log(buf.toString());
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
